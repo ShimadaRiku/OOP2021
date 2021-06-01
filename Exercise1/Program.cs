ccb4c102a6ce644cea3bf1ffab131df691fd7b84
RikuShimada <32131@ojs.ac.jp>
2021-06-01 09:32:11 +0900

    P79 問題2-1完成

---
+++ Exercise1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1 {
+    class Program {       
+        static Song[] songs = new Song[2];
+        static void Main(string[] args) {
+            songs[0] = new Song("サウダージ", "ポルノグラフティ", 250);
+            songs[1] = new Song("韻波句徒", "CHEHON", 320);
+
+            PrintSongs(songs);
+
+        }
+
+        private static void PrintSongs(Song[] songs) {
+            
+            foreach (var song in songs) {
+                int min = song.Length1 / 60;
+                int sec = song.Length1 % 60;
+                    Console.WriteLine("{0},{1},{2}:{3}", song.Title1,song.ArtistName1,min,sec);
+            }
+        }
+    }
+}
---
+++ Exercise1/Song.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1 {
+    class Song {
+        string Title;
+        string ArtistName;
+        int Length;
+
+        public string Title1 { get => Title; set => Title = value; }
+        public string ArtistName1 { get => ArtistName; set => ArtistName = value; }
+        public int Length1 { get => Length; set => Length = value; }
+
+        public Song(string title,string artistName,int length) {
+            this.Title = title;
+            this.ArtistName = artistName;
+            this.Length = length;
+        }
+    }
+
+}
