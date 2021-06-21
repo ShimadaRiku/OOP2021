767a887e0678debf6b9144388117c48871ab98e9
RikuShimada <32131@ojs.ac.jp>
2021-06-21 14:06:15 +0900

    １期目テスト

--- TestProject/Test01/ScoreCounter.cs
+++ TestProject/Test01/ScoreCounter.cs
@@ -7,33 +7,39 @@ namespace Test01 {
 
         // コンストラクタ
         public ScoreCounter(string filePath) {
+            _score = ReadScore(filePath);
 
-
-
-            
         }
 
 
         //メソッドの概要： 
         private static IEnumerable<Student> ReadScore(string filePath) {
-            
-
-
-
-
-
-
-            
+            List<Student> sales = new List<Student>();
+            string[] lines = File.ReadAllLines(filePath);
+            foreach (string line in lines) {
+                string[] items = line.Split(',');
+                Student student = new Student{
+                    Subject = items[0],
+                    Name = items[1],
+                    Score = int.Parse(items[2])
+                };
+                sales.Add(student);
+            }
+            return sales;
         }
-
         //メソッドの概要： 
         public IDictionary<string, int> GetPerStudentScore() {
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+            foreach (Student student in _score) {
+                if (dict.ContainsKey(student.Name))
+                    //すでにコレクションに店舗が設定されている
+                    dict[student.Name] += student.Score;
+                else
+                    //コレクションへ店舗を登録
+                    dict[student.Name] = student.Score;
+            }
+            return dict;
 
-
-
-
-
-            
         }
     }
 }
--- TestProject/Test01/Student.cs
+++ TestProject/Test01/Student.cs
@@ -1,6 +1,10 @@
 ﻿namespace Test01 {
     class Student {
-
+        
+        public string Name { get; set; }
+        public string Subject { get; set; }
+        public int Score { get; set; }
+        
 
 
     }
