3ea62a3b57bd9519d2527cb6a1cc3d8746ff21b7
infosys <infosys@C4223>
2021-11-17 15:15:55 +0900

    NumberGame  タイマー

--- NumberGame/MainWindow.xaml.cs
+++ NumberGame/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace NumberGame
 {
@@ -20,22 +22,53 @@ namespace NumberGame
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        DispatcherTimer dt = new DispatcherTimer();
+        Stopwatch sw = new Stopwatch();
+        int rand = 0;
         public MainWindow()
         {
-            
             InitializeComponent();
-            random(1, 25);
+            random(1, 26);
+            this.Title = "NumberGame タイム[00:00:000]";
+            dt.Interval= new TimeSpan(0, 0, 0, 0, 1);
+            dt.Tick += Dt_Tick;
+            dt.Start();
+            sw.Start();
         }
+
+        private void Dt_Tick(object sender, EventArgs e)
+        {
+            this.Title = "NumberGame タイム[" + sw.Elapsed.ToString(@"mm\:ss\:fff") + "]";
+        }
+
         public void random(int min,int max)
         {
-            //int min = 0;
-            //int max = 25;
+            Random r1 = new System.Random();
+            rand = r1.Next(min, max);
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             Button button = e.Source as Button;
             button.Background = Brushes.CadetBlue;
-        }
+
+            int select = int.Parse((string)button.Content);
+
+            if (rand == select)
+            {
+                Ans.Text = "正解です";
+                dt.Stop();
+                sw.Stop();
+            }
+            else if (rand > select)
+            {
+                Ans.Text = select.ToString() + "より大きい数です";
+            }
+            else if (rand < select)
+            {
+                Ans.Text = select.ToString() + "小さい数です";
+            }
+        }   
     }
 }
